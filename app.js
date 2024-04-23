57c48e3a1294ec1f574c233640dd4441ad2da32e
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-23 22:47:26 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // write code here
+function deleteTask(id) {
+  tasks = tasks.filter(task => task.id !== id);
+}
+
+const tasks = [
+  { id: 1, name: 'Fishing' },
+  { id: 2, name: 'Shopping' },
+  { id: 3, name: 'Swimming' }
+];
+
+deleteTask(2);
+console.log(tasks);
+
